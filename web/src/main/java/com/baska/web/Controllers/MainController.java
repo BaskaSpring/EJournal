02f43a2ec6923bdee92f7858e0d1236b859d062e
Baska <80294496+BaskaSpring@users.noreply.github.com>
2022-04-03 20:14:10 +0900

    HLC

--- web/src/main/java/com/baska/web/Controllers/MainController.java
+++ web/src/main/java/com/baska/web/Controllers/MainController.java
@@ -15,6 +15,7 @@ public class MainController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+
     }
 
 }
